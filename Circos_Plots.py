7c982d6d622035d04b6de13dbb471dd2e554faff
Stanislav Listopad <slistopa@uci.edu>
2024-11-07 16:06:50 -0800

    Latest Circos Code

--- Circos_Plots.py
+++ Circos_Plots.py
@@ -13,10 +13,14 @@ import numpy as np
 
 Garc    = pycircos.Garc
 Gcircle = pycircos.Gcircle
-
+'''
 colors = ['darkred', 'red', 'coral', 'peru', 'darkorange', 'orange', 'goldenrod', 'gold', 'olive', 
           'lawngreen', 'forestgreen', 'seagreen', 'lightseagreen', 'teal', 'cyan', 'deepskyblue', 
           'dodgerblue', 'darkblue', 'blueviolet', 'indigo', 'purple', 'magenta', 'deeppink', 'crimson']
+'''
+colors = ['lightcoral', 'red', 'coral', 'peru', 'darkorange', 'orange', 'goldenrod', 'gold', 'yellow', 
+          'lawngreen', 'forestgreen', 'seagreen', 'lightseagreen', 'paleturquoise', 'cyan', 'deepskyblue', 
+          'dodgerblue', 'cornflowerblue', 'blueviolet', 'mediumorchid', 'hotpink', 'magenta', 'deeppink', 'crimson']
 indir = 'C:/Stas/LabWork/Bioinformatics/Projects/Ch5_NA_Cohort/Circos/example_notebooks/sample_data/'
 #Set chromosomes
 circle = Gcircle() 
@@ -63,15 +67,14 @@ circle.figure
 '''
 
 #linkplot
-indir = 'C:/Stas/LabWork/Bioinformatics/Projects/Ch5_NA_Cohort/REMMA_Results/Combined_Set_Biclustering/'
+indir = 'C:/Stas/LabWork/Bioinformatics/Projects/Ch5_NA_Cohort/REMMA_Results/Combined_Fixed_Set_Biclustering/'
 values_all   = [] 
 arcdata_dict = collections.defaultdict(dict)
 with open(indir + "biclustering_results_all.txt") as f:
-    f.readline()
+    #f.readline()
     i = 0
     for line in f:
-        #if(i < 5):
-        #print(line)
+
         line  = line.rstrip().split(",")
         name1  = line[0]     
         start1 = int(line[1])-1
@@ -85,6 +88,49 @@ with open(indir + "biclustering_results_all.txt") as f:
         circle.chord_plot(source, destination, facecolor=circle.garc_dict[name1].facecolor,
                           edgecolor=circle.garc_dict[name1].facecolor, linewidth=0.15)
         i += 1
+ 
+    
+gene_loci_all = []
+indir = 'C:/Stas/LabWork/Bioinformatics/Projects/Ch5_NA_Cohort/AUD_Resources/Start_Sets/Expanded_Ranges/'
+with open(indir + "Combined_Set_score900_db001_and_exp001_1_and_genehancer_score25_ranges.txt") as f2:
+    i = 0
+    for line in f2:
         
+        line  = line.rstrip().split(" ")
+        name = line[0]
+        start1 = int(line[1])
+        end1 = int(line[2])
+        gene_loci_all.append([name,start1,end1])
+        
+        i += 1
+        
+#print(gene_loci_all)
+    
+i = 1
+while i <= 22:
+    gene_loci_current = []
+    for gene_loci in gene_loci_all:
+        if(gene_loci[0] == str(i)):
+            gene_loci_current.append(gene_loci)
+            
+           
+    if(len(gene_loci_current) == 0):
+        i += 1
+        continue
+    arc_name = 'chr' + str(i)
+    data = list()
+    positions = list()
+    widths = list()        
+    for gene_loci in gene_loci_current:
+        data.append(1)
+        positions.append(gene_loci[1])
+        widths.append(gene_loci[2] - gene_loci[1])
+        
+    
+    circle.barplot(arc_name, data, positions, widths, (950,1000),
+                   facecolor = 'black', edgecolor = 'black', linewidth = 0.08)
+        
+    i += 1
+
 circle.figure
 plt.savefig('filename.png', dpi=300, bbox_inches="tight")
